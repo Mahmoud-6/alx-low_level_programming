869d8f08bff08aa4402483aeaadbc86faacad941
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-16 19:31:14 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+/* main functoin
+ * put function using 
+ * returning 0 
+ */
+int main(vid)
 {
   puts("/"Programming is like building a multilingual puzzle");
 
