ad3d68efece6570b0c0475015ff7d7f47ec2b0a2
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-17 13:40:18 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ *main - basic function
+ *Return: 0
+ */
+int main(void)
+{
+for (char c= 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar("\n");
+return (0);
+}
