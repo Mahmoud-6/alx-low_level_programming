c2870651ce66cd5a52c2be754777d444ff13fc0b
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-16 21:39:13 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main - basic function
  *Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
