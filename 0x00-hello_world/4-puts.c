f19c333fe29048e4ebe281bf488235d39a77bdcf
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-16 19:37:09 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-/* main functoin
- * put function using 
- * returning 0 
- */
+/** main -start functoin
+ * 
+ * return : 0
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-
-
   return 0;
 }
