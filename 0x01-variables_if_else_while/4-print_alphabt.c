b243e1d783fce823db285307909ae24bf2b880bc
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-17 14:40:15 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * *main - basic function
+ * *Return: 0
+ */
+int main(void)
+{
+char n;
+for (n = 'a' ; n <= 'z' ; n++)
+{
+if (n != 'e' && n != 'q')
+{
+putchar(n);
+}
+}
+putchar('\n');
+return (0);
+}
