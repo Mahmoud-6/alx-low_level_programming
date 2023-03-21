24f68529a533a9c0d7e80a9404825a2021a15255
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 14:59:29 -0700

    c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
 * _islower - function returns 0 or 1
-* @c: parameter tha caontains the value
+* @c: parameter that caontains the value
 * Return: 0 or 1
 */
 int _islower(int c)
