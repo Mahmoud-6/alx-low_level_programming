dcd5840b3f03794c1e154894f237b53700fd52ae
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 09:37:13 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* *main - basic function
+*Return: 0
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
-for (int i = 'a' ; i <= 'z' ; i++)
+for (char i = 'a' ; i <= 'z' ; i++)
 {
 _putchar(i);
 }
