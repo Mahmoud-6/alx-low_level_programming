26126de3aa091a419294c6112a07e1ad76dfe9e9
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 15:36:42 -0700

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,19 +7,23 @@
 */
 int print_sign(int n)
 {
+int x;
 if (n > 0)
 {
-_putchar('+');
+x = '+';
+return(x);
 }
 else
 {
 if (n < 0)
 {
-_putchar('-');
+x = '-';
+return(x);
 }
 else
 {
-_putchar('0');
+x = 0;
+return(x);
 }
 }
 }
