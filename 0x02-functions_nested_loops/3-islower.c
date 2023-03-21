4767f4ef82f4fdda66bb23a714de2615f9e1d641
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 14:43:55 -0700

    c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _islower - function returns 0 or 1
+* @c: parameter tha caontains the value
+* Return: 0 or 1
+*/
+int _islower(int c)
+{
+if (c == 'c')
+{
+return (1);
+}
+else
+{
+return (0);
+}
