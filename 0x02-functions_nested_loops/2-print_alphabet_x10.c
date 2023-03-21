0ad2ff9e9adb0c0fd9d0c958b31ced2ff97bc624
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 10:31:24 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
 * *main - basic function
 *Return: 0
 */
-void print_alphabet_x10(void);
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
+void print_alphabet_x10(0);
 void print_alphabet_x10(void)
 {
 char i;
