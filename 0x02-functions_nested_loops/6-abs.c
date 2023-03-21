d0ccc9fd78d844ae35ab3bed0040ea2227a31fb1
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 16:13:22 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - function that returns absolute
+* @n: parameters that contains the value
+* Return: 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * (-1);
+return (n);
+}
+else
+{
+return (n);
+}
+}
