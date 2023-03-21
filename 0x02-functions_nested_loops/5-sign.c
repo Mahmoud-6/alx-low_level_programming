3ee103cf10ce4e0bd739045f9fa7b3314883d67d
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 15:30:30 -0700

    c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - function returns + or - or 0
+* Return: 0 or 1
+* @n: parameter that comtains value
+*/
+int print_sign(int n)
+{
+if (c > 0)
+{
+_putchar('+');
+}
+else
+{
+if (c < 0)
+{
+_putchar('-');
+}
+else
+{
+_putchar('0');
+}
+}
+}
