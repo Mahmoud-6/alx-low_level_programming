0cf7d47bd01f241b3065cea0aad527a291cd3a13
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-21 12:25:17 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include "main.c"
-void print_alphabet_x10();
 /**
 * *main - basic function
 *Return: 0
@@ -11,7 +9,7 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-void print_alphabet_x10(void)
+extern void print_alphabet_x10(void)
 {
 char i;
 int j;
