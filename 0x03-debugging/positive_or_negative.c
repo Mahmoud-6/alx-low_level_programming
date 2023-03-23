296ade15c566af61eb880cadc1518a073d369503
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 03:32:27 -0700

    c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,11 +5,8 @@
  *main - basic function
  *Return: 0
  */
-int positive_or_negative(void)
+void positive_or_negative(int n)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
@@ -22,5 +19,4 @@ else
 {
 printf("%d is negative\n", n);
 }
-return (0);
 }
