837aeafc0f2246fecdb19ccffed026a3e5ac6f6d
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 07:36:42 -0700

    c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - function
+ * @c: variable that contains a value
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+return (0);
+}
