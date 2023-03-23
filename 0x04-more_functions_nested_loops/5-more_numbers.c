21932d2451e815c66a50eb46ef2313370c4ee25a
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 08:51:38 -0700

    c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+ #include "main.h"
+/**
+ * more_numbers - function
+ * Return: null
+ */
+void more_numbers(void)
+{
+int i, j ;
+for (i = 0 ; i <= 10 ; i++)
+{
+for (j = 0 ; j <=9 ; j++)
+{
+_putchar('0' + j);
+}
+_putchar('\n');
+}
+}
