e78e7fdf0c4f52f6da8dcc4ae5ecc2204198f903
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 00:25:06 -0700

    c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * main - basic function
- * Return: 0
+ *main - basic function
+ *Return: 0
  */
 int main(void)
 {
-int i = 0;
-postitive_or_negative(i);
+int p;
+p = 0;
+postitive_or_negative(p);
 return (0);
 }
