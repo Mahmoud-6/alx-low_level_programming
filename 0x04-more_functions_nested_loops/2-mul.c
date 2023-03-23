317cbffedecfc785a7a28fd8085765d6f07c7fff
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 07:45:20 -0700

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function
+ * @a: variable that contains a value
+ * @b: variable that contains a value
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
