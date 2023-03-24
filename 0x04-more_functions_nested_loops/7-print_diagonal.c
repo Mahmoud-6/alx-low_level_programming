200220374b4a12553d4492a22cf44e968d9934d5
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-23 17:04:02 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - function
+ * @n: variable that contians value
+ * Return: null
+ */
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j < i ; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
