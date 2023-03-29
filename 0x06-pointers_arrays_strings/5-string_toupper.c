10e0ec3d8b8fd0fde2592b5617214fd845b49103
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-29 16:37:59 -0700

    c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * tring_toupper - function
+ * @a: variable that contain value
+ * Return: a
+ */
+char *string_toupper(char *a)
+{
+int i;
+for (i = 0 ; a[i] != '\0' ; i++)
+{
+if (a[i] >= 'a' && a[i] <= 'z')
+{
+a[i] -= 32;
+}
+}
+return (a);
+}
