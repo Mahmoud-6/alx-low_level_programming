78fdf1a3eeacc0487d9aa52756adbb217ed42dc9
Mahmoud-6 <mh.khaled656@gmail.com>
2023-03-30 00:05:10 -0700

    c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * cap_string - function
- * @a: variable that contain value
- * Return: r
- */
-char *cap_string(char *a)
+  * cap_string - ...
+  * @s: ...
+  *
+  * Return: char value
+  */
+char *cap_string(char *s)
 {
-int i;
-for (i = 'a' ; a[i] != '\0' ; i++)
-{
-if (a[i] >= 'a' && a[i] <= 'z')
-{
-a[i] -= 32;	}
-}
-return (a);
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		a++;
+	}
+
+	return (s);
 }
