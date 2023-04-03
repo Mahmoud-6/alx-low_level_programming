efe65f065080c3dfd483440c3944418a5acff08d
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-03 06:57:01 -0700

    c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strpbrk - function
+* @s: variable that contains value
+* @accept: variable that contains value
+* Return: s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int c = 0, p = 0;
+while (s[c] != '\0')
+{
+while (accept[p] != '\0')
+{
+if (s[c] == accept[p])
+{
+s += c;
+return (s);
+}
+p++;
+}
+c++;
+}
+return (0);
+}
