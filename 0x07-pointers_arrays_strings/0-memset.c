1cf2892ac4220790df5e2ab4d74f7180fed39fbf
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-03 06:12:04 -0700

    c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _memset - function
+* @s: variable that contains value
+* @b: variable that contains value
+* @n: variable that contains value
+* Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+s[i] = b;
+}
+return (s)
+}
