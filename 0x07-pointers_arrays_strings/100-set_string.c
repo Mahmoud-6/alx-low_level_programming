1cafde56e80a75e02dcd7f0d2cf51d9029288b6b
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-03 08:28:34 -0700

    c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*  set_string - function
+* @s: variable that contains value
+* @to: variable that contains value
+* Return: null
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
