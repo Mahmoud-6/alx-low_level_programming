1ec1a0bdbac27ea86baddc213813fcb6fc50311a
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-08 13:29:51 -0700

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf ("%d", argc - 1);
+return (0);
+}
