c96c908f589aa856a8f7e4fbea7c7c92c67ec5b7
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-13 18:58:36 -0700

    c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,13 +8,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int i = 0, sum;
 char *p;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-sum = nmemb * size;
 p = calloc(nmemb, size);
 return (p);
 }
