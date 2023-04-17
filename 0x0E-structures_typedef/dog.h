0bcfefa5cadb4cc0ab902b98a1cf1ab67f597999
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-17 05:05:46 -0700

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdio.h>
+/**
+ *struct dog - function
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 }
+#endif
