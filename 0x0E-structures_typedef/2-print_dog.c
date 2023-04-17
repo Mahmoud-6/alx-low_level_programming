a64cc4c05f3501ff82079f1edaf4f46fbce4e9f7
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-17 15:29:56 -0700

    c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - function
+ * @d: variable
+ */
+void print_dog(struct dog *d)
+{
+if (d)
+{
+printf("Name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
+}
+}
