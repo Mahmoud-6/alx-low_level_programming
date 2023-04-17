176347bed695742abadea28e427a56e292782634
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-17 05:25:58 -0700

    c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
 /**
  * struct dog - function
@@ -8,10 +8,14 @@
  * @owner: variable
  * Description: information about the dog
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-}
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
