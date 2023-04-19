a333dfe7df879f2a3d70dffc5588530cb2ef0b94
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-19 16:09:28 -0700

    v

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * get_op_func - function
+ * @s: value
+ * Return: result
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+{"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+int i;
+while (i < 5)
+{
+if (*(ops[i].op) == *s && s[0] != '\0')
+return (ops[i].f);
+i++;
+}
+i = 0;
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - function
+ * @a: value
+ * @b: value
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub -  function
+ * @a: value
+ * @b: value
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - function
+ * @a: value
+ * @b: value
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - function
+ * @a: value
+ * @b: value
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+/**
+ * op_mod - function
+ * @a: value
+ * @b: value
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
