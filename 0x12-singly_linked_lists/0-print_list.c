803ab964d4284d288ef3aa185ee0761754e05098
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-27 15:19:03 -0700

    c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * print_list - function
+  * @h: value
+  * Return: result
+  */
+size_t print_list(const list_t *h)
+{
+size_t i;
+for (i = 0; h; i++)
+{
+printf("[%d] %s\n", (*h).len, (*h).str);
+h = (*h).next;
+}
+return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+1-list_len.c : function that returns the number of elements in a linked list_t list.
