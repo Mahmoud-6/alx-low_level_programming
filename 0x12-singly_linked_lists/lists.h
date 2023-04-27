64f198f042f0509b1d05464de3099c93c92897af
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-27 16:15:04 -0700

    c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+char *strdup(const char *string);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
