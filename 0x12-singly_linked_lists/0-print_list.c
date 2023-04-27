1fd14e8b5844a38022937d3d5835aaec8121453e
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-27 15:32:38 -0700

    c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,8 +10,8 @@ size_t print_list(const list_t *h)
 size_t i;
 for (i = 0; h; i++)
 {
-printf("[%d] %s\n", (*h).len, (*h).str);
-h = (*h).next;
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
 }
 return (i);
 }
