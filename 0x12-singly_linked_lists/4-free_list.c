11e84dd0697c5e765039cd239da70c57f98246af
Mahmoud-6 <mh.khaled656@gmail.com>
2023-04-27 16:26:07 -0700

    c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - function
+ * @head: value
+ * Return: NULL
+ */
+void free_list(list_t *head)
+{
+list_t *c, *n;
+c = head;
+while (c != NULL)
+{
+n = c->n;
+free(c->str);
+free(c);
+c = n;
+}
+}
