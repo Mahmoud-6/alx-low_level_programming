04482a0de3ac4cfe6514f8fd824b6f3eee8c8b96
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-01 14:52:45 -0700

    c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - function
+ * @head: value
+ */
+void free_listint(listint_t *head)
+{
+listint_t *k;
+while (head)
+{
+k = head->next;
+free(head);
+head = k;
+}
+}
