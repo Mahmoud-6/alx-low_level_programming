0c7c75d226e230c8fcd0cb7ed6a8bfd2baaaee2b
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-02 14:43:44 -0700

    c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - function
+ * @head: value
+ * Return: result
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *k;
+int a;
+if (*head == NULL)
+return (0);
+k = *head;
+a = (*head)->n;
+*head = (*head)->next;
+return (a);
+}
