59912ab29962472971ef35840d921955416bd59f
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-02 09:49:00 -0700

    c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - function
+ * @head: value
+ * Return: NULL
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *k;
+if (head == NULL)
+{
+return;
+}
+while (head)
+{
+k = (*head)->next;
+free(*head);
+*head = k;
+head = NULL;
+}
+}
