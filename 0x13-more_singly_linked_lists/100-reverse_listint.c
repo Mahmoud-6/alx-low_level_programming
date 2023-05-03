b3e1d4d7bd5aabf41015e350ec951558460c127d
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-02 17:08:38 -0700

    c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@ listint_t *reverse_listint(listint_t **head)
 listint_t *a, *b;
 if (head == NULL || *head == NULL)
 return (NULL);
-be = NULL;
+b = NULL;
 while ((*head)->next != NULL)
 {
 a = (*head)->next;
