23564f18b711cd48d1255b9d6d139d430fa0233b
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-08 17:59:08 -0700

    c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * read_textfile - function
+ * @filename: value
+ * @text_content: value
+ * Return: result
+ */
+int append_text_to_firle(const char *filename, char *text_content)
+{
+int ope, write, l = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (l = 0; text_content[len];)
+l++;
+}
+ope = open(filename, O_WRONLY | O_APPEND);
+write = write(ope, text_content, l);
+if (ope == -1 || write == -1)
+return (-1);
+close(ope);
+return (1);
+}
