5716b6350651ad389e6674b1ea389f523f06a571
Mahmoud-6 <mh.khaled656@gmail.com>
2023-05-08 18:42:29 -0700

    c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,10 +16,10 @@ int main(int argc, char **argv)
 int f, t, on_c, writ, rea;
 char buffer[1024];
 if (argc != 3)
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (t == -1)
-dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit();
+dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
 f = open(argv[1], O_RDONLY);
 if (f == -1)
 dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
@@ -27,20 +27,21 @@ while (1)
 {
 rea = read(f, buffer, 1024);
 if (rea == -1)
-dprintf(STDERR_FILENO, READ_ERR, argv[1]), (98);
+dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 if (rea > 0)
 {
-writ = write(to, buffer, r);
+writ = write(t, buffer, rea);
 if (writ == -1)
-dprintf(STDERR_FILENO, WRITE_ERR, argv[2]),exit(99);
-} else
+dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+}
+else
 break;
 }
 on_c = close(f);
 if (on_c == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f),exit(100);
-on_close = close(to);
-if (on_close == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", t),exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f), exit(100);
+on_c = close(t);
+if (on_c == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", t), exit(100);
 return (0);
 }
